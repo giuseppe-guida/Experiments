1bb35f65f2a785bc7582f9d4e5c9fb22bdf96b38
yblaze <peppe.guida@gmail.com>
2014-11-05 18:41:18 +0100

    First_committment
    Directory for experimental scripts

---
+++ Function caller.R
@@ -0,0 +1,8 @@
+# TODO: Add comment
+# 
+# Author: g.guida
+###############################################################################
+
+source("C:/Users/g.guida/workspace/Experiments/my_function.R");
+x <- my_function();
+print(x);
---
+++ Reading_datetime.R
@@ -0,0 +1,30 @@
+# TODO: Add comment
+# 
+# Author: g.guida
+###############################################################################
+
+
+#custom function to read from data
+setClass("myDate", representation(date = "character"));
+setClass("myTime", representation(time = "character"));
+setAs("character","myDate", function(from) as.Date(from, format="%d/%m/%Y") )
+setAs("character","myTime", function(from) as.POSIXct(from, format="%H:%M:%S") )
+
+tmp <- c("1, 15/08/2008", "2, 23/05/2010")
+con <- textConnection(tmp)
+tmp2 <- read.csv(con, colClasses=c('numeric','myDate'), header=FALSE)
+str(tmp2)
+
+tmp <- c("1, 17:24:00", "2, 17:24:00")
+con <- textConnection(tmp)
+tmp2 <- read.csv(con, colClasses=c('numeric','myTime'), header=FALSE)
+str(tmp2)
+
+
+#read from file
+hpc = read.table(file, header = TRUE, sep = ";", 
+		dec = ".",  
+		na.strings = "?",
+		colClasses = c("myDate", "myTime", rep("numeric", 7)),
+		strip.white = TRUE, blank.lines.skip = TRUE
+);
